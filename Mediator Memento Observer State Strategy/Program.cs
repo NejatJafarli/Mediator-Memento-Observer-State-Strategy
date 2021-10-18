e7fe3d5bd991d810d3f5064c41230a9550c36c30
nicat ceferli <mega.ceferli@gmail.com>
2021-10-18 21:51:16 +0400

    first commit

---
+++ Mediator Memento Observer State Strategy/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Mediator_Memento_Observer_State_Strategy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine();
+            Console.WriteLine("Hellog");
+            Console.WriteLine("My Prop");
+            Console.WriteLine("My Pdddd");
+        }
+    }
+}
