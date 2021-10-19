8b483cebfcd5ef0039eeafd52ac6e4cbeeba49a8
nicat ceferli <mega.ceferli@gmail.com>
2021-10-19 14:18:38 +0400

    Mediator Memento Ready

---
+++ Mediator/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Mediator
+{
+    public interface IAirport
+    {
+        void Register(AirlineBase airline);
+        bool GiveLandingPermission(string flightCode);
+    }
+    public abstract class AirlineBase
+    {
+        public string FlightCode { get; set; }
+        public IAirport Airport { get; set; }
+
+        public AirlineBase(string flightCode)
+        {
+            FlightCode=flightCode;
+        }
+
+        public virtual void GetLandingPermission()
+        {
+            if (Airport != null)
+                if (Airport.GiveLandingPermission(FlightCode))
+                    Console.WriteLine("Enis izni verildi.");
+                else
+                    Console.WriteLine("Enis izni verilmedi!");
+            else
+                Console.WriteLine("Bu Air plane Qeydiyatta yoxdur ona gorede Enis Izni verilmedi");
+        }
+    }
+    public class AtaturkAirport : IAirport
+    {
+        private Dictionary<string, AirlineBase> _planes;
+
+        public AtaturkAirport()
+        {
+            _planes = new Dictionary<string, AirlineBase>();
+        }
+
+        public void Register(AirlineBase airline)
+        {
+            if (!_planes.ContainsValue(airline))
+                _planes.Add(airline.FlightCode, airline);
+
+            airline.Airport = this;
+        }
+
+        public bool GiveLandingPermission(string flightCode)
+        {
+            if (_planes.ContainsKey(flightCode))
+                return true;
+            else
+                return false;
+        }
+    }
+    public class THYAirline : AirlineBase
+    {
+        public THYAirline(string flightCode) : base(flightCode)
+        {
+        }
+
+        public override void GetLandingPermission()
+        {
+            Console.WriteLine(FlightCode + " Nolu THY Airplane Enis izni Istedi.");
+
+            base.GetLandingPermission();
+        }
+    }
+    public class PegasusAirline : AirlineBase
+    {
+        public PegasusAirline(string flightCode) : base(flightCode)
+        {
+        }
+
+        public override void GetLandingPermission()
+        {
+            Console.WriteLine(FlightCode + " Nolu Pegasus Airplane Enis izni Istedi.");
+            base.GetLandingPermission();
+        }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            AtaturkAirport AtaturkAirport = new AtaturkAirport();//airportumuzu yaradiriq
+
+
+            THYAirline thyAirline1 = new THYAirline("C234");
+
+            THYAirline thyAirline2 = new THYAirline("C240");
+
+            PegasusAirline pegasusAirline1 = new PegasusAirline("E250");
+
+            //bu airport a aid Airplaneleri qeydiyata saliriq 
+            AtaturkAirport.Register(thyAirline1);
+
+
+            AtaturkAirport.Register(pegasusAirline1);
+
+            thyAirline2.GetLandingPermission();// qeydiyattan kecmeyib deye icaze verilmir
+
+            Console.WriteLine();
+            thyAirline1.GetLandingPermission();//Icaze verildi
+            Console.WriteLine();
+
+            pegasusAirline1.GetLandingPermission();//icaze verildi
+            Console.WriteLine();
+
+
+        }
+    }
+}
---
+++ Memento/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace Memento
+{
+    class SettingsOrginator
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public bool RememberMe { get; set; }
+
+        public SettingsMemento Backup() => new SettingsMemento(Username, Password, Email, RememberMe);
+        public void Setup(SettingsMemento settings)
+        {
+            this.Username = settings.Username;
+            this.Password = settings.Password;
+            this.Email = settings.Email;
+            this.RememberMe = settings.RememberMe;
+        }
+        public SettingsOrginator(string userName, string password, string email, bool rememberMe)
+        {
+            Username = userName;
+            Password = password;
+            Email = email;
+            RememberMe = rememberMe;
+        }
+    }
+    public class SettingsMemento
+    {
+        public SettingsMemento(string username, string password, string email, bool rememberMe)
+        {
+            Username = username;
+            Password = password;
+            Email = email;
+            RememberMe = rememberMe;
+        }
+
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+        public bool RememberMe { get; set; }
+    }
+
+    public class SettingsCaretaker
+    {
+        public SettingsMemento User { get; set; }
+    }
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            SettingsOrginator User = new SettingsOrginator("Nicat", "12345", "Mega.ceferli@gmail.com", true);
+
+            Console.WriteLine($"Name {User.Username}\nPassword {User.Password}\nEmail {User.Email}\nRemember me {User.RememberMe}");
+            Console.WriteLine();
+            SettingsCaretaker settingsCaretaker=new SettingsCaretaker{
+                User = User.Backup()
+            };// burdada bir backup saxlayiriq
+
+            //burda propertylerimizi deyistiririk
+            User.Email = "AnyEmail@gmail.com";
+            User.RememberMe = false;
+            User.Username = "Some Guy";
+            User.Password= "Password";
+
+            Console.WriteLine($"Name {User.Username}\nPassword {User.Password}\nEmail {User.Email}\nRemember me {User.RememberMe}");
+            Console.WriteLine();
+
+
+            //sonra evvelki halina qayitsin isteyirik
+
+            User.Setup(settingsCaretaker.User);
+
+
+            Console.WriteLine($"Name {User.Username}\nPassword {User.Password}\nEmail {User.Email}\nRemember me {User.RememberMe}");
+            Console.WriteLine();
+
+        }
+    }
+}
